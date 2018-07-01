aee2ff2bb728b1c896e0726910560a623228fe0d
datashaman <marlinf@datashaman.com>
2018-07-01 21:33:12 +0200

    neaten things up

--- setup.py
+++ setup.py
@@ -1,30 +1,33 @@
 from setuptools import setup
 
-setup(name='activerest',
-      version='0.0.1',
-      description='Python REST resource client, modeled on Ruby on Rails\' ActiveResource.',
-      author='Marlin Forbes',
-      author_email='marlinf@datashaman.com',
-      url='https://github.com/datashaman/activerest/',
-      packages=['activerest'],
-      license='MIT License',
-      install_requires=[
-          'requests',
-          'six',
-      ],
-      tests_require=[
-          'requests-mock',
-      ],
-      test_suite='tests',
-      platforms=['any'],
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.6',
-                   'Topic :: Software Development',
-                   'Topic :: Software Development :: Libraries',
-                   'Topic :: Software Development :: Libraries :: Python Modules']
+setup(
+    name='activerest',
+    version='0.0.1',
+    description='Python REST resource client, modeled on Ruby on Rails\' ActiveResource.',
+    author='Marlin Forbes',
+    author_email='marlinf@datashaman.com',
+    url='https://github.com/datashaman/activerest/',
+    packages=['activerest'],
+    license='MIT License',
+    install_requires=[
+        'requests',
+        'six',
+    ],
+    tests_require=[
+        'requests-mock',
+    ],
+    test_suite='tests',
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
