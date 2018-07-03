ba15a759e31ad65da2e0998ed2eb2f14a806361b
datashaman <marlinf@datashaman.com>
2018-07-03 20:35:12 +0200

    wip

--- activerest/resources.py
+++ activerest/resources.py
@@ -1,16 +1,20 @@
-import inflection
-import logging
+"""
+Python REST client, modeled on Ruby on Rails' ActiveResource
+"""
+
 import requests
+import inflection
 
-from collections import namedtuple
 from six.moves.urllib.parse import urlencode
-
-log = logging.getLogger(__name__)
 
 
 class Resource(object):
     def __init__(self, _meta=None, **attributes):
-        instance_defaults = dict((k, v) for k, v in self.__class__.__dict__.items() if k != 'Meta' and k[0] != '_')
+        instance_defaults = {}
+
+        for key, value in self.__class__.__dict__.items():
+            if key != 'Meta' and key[0] != '_':
+                instance_defaults[key] = value
 
         self.__dict__.update(instance_defaults)
         self.__dict__.update(attributes)
@@ -26,35 +30,52 @@ class Resource(object):
         self._meta.update(_meta)
 
     def __repr__(self):
-        return '%s(%s)' % (self.__class__.__name__, str(' '.join('%s=%s' % (k, repr(v)) for k, v in self.__dict__.items() if k[0] != '_')))
+        parts = {}
+
+        for key, value in self.__dict__.items():
+            if key[0] != '_':
+                parts[key] = repr(value)
+
+        string = ' '.join(['%s=%s' % (key, value) for key, value in parts.items()])
+
+        return '%s(%s)' % (self.__class__.__name__, string)
+
 
     @property
     def attributes(self):
+        """Attributes on the resource."""
         return dict((k, v) for k, v in self.__dict__.items() if k[0] != '_')
 
     def primary_key(self):
+        """The primary key value."""
         return self.__dict__[self.__class__.pk()]
 
     def is_new(self):
+        """Is the resource new, ie unsaved."""
         return not self._meta['persisted']
 
     def is_persisted(self):
+        """Is the resource persisted, ie saved."""
         return self._meta['persisted']
 
     def load(self, attributes):
-        for k, v in attributes.items():
-            if k[0] != '_':
-                setattr(self, k, v)
+        """Set the attributes on the resource."""
+        for key, value in attributes.items():
+            if key[0] != '_':
+                setattr(self, key, value)
 
     def update_attribute(self, name, value):
+        """Update a single attribute and save the resource."""
         setattr(self, name, value)
         return self.save()
 
     def update_attributes(self, attributes):
+        """Update a dictionary of attributes and save the resource."""
         self.load(attributes)
         return self.save()
 
     def save(self):
+        """Save the resource by calling the API."""
         if self.is_new():
             path = self.__class__.collection_path()
             method = 'POST'
@@ -63,12 +84,10 @@ class Resource(object):
             method = 'PUT'
 
         data = self._transform_params(self.attributes)
-        response = self.__class__._request(method, path, data=data)
+        response = self.__class__.request(method, path, data=data)
 
-        if (
-            method == 'POST' and response.status_code == 201
-            or method == 'PUT' and response.status_code == 200
-        ):
+        if (method == 'POST' and response.status_code == 201
+                or method == 'PUT' and response.status_code == 200):
             self._meta['persisted'] = True
             self.load(response.json())
             return True
@@ -76,6 +95,7 @@ class Resource(object):
         return False
 
     def destroy(self):
+        """Delete the resource by calling the API."""
         if self.is_persisted() and self.__class__.delete(self.primary_key()):
             self._meta['persisted'] = False
             return True
@@ -84,36 +104,41 @@ class Resource(object):
 
     @classmethod
     def pk(cls):
+        """Name of the primary key attribute."""
         return getattr(cls.Meta, 'primary_key', 'id')
 
     @classmethod
     def all(cls):
+        """Return all resources from the API."""
         return cls.find()
 
     @classmethod
-    def delete(cls, id):
-        path = cls.element_path(id)
-        response = cls._request('DELETE', path)
+    def delete(cls, identifier):
+        """Delete a single resource by identifier."""
+        path = cls.element_path(identifier)
+        response = cls.request('DELETE', path)
         return response.status_code == 200
 
     @classmethod
-    def exists(cls, id):
-        path = cls.element_path(id)
-        response = cls._request('HEAD', path)
+    def exists(cls, identifier):
+        """Check if a single resource exists by identifier."""
+        path = cls.element_path(identifier)
+        response = cls.request('HEAD', path)
         return response.status_code == 200
 
     @classmethod
-    def find(cls, id=None, params=None, **query_options):
-        if id:
-            path = cls.element_path(id)
+    def find(cls, identifier=None, params=None):
+        """Find resources by ID or by query options."""
+        if identifier:
+            path = cls.element_path(identifier)
         else:
             if params is None:
                 params = {}
             path = cls.collection_path(**params)
 
-        result = cls._request('GET', path).json()
+        result = cls.request('GET', path).json()
 
-        if id:
+        if identifier:
             if result:
                 return cls(_meta={'persisted': True}, **result)
             return None
@@ -128,7 +153,7 @@ class Resource(object):
         transformed = {}
 
         for key, value in params.items():
-            if type(value) == bool:
+            if isinstance(value, bool):
                 value = 'true' if value else 'false'
 
             transformed[key] = value
@@ -137,13 +162,15 @@ class Resource(object):
 
     @classmethod
     def collection_name(cls):
+        """Collection name used in generating collection path."""
         if hasattr(cls.Meta, 'collection_name'):
             return cls.Meta.collection_name
-        else:
-            return inflection.dasherize(inflection.underscore(inflection.pluralize(cls.__name__)))
+
+        return inflection.dasherize(inflection.underscore(inflection.pluralize(cls.__name__)))
 
     @classmethod
     def query_string(cls, query_options=None):
+        """Generate query string from query options."""
         if query_options:
             query_options = cls._transform_params(query_options)
             return '?%s' % urlencode(query_options)
@@ -151,14 +178,17 @@ class Resource(object):
 
     @classmethod
     def collection_path(cls, **query_options):
+        """Path to the collection API endpoint."""
         return '/%s%s' % (cls.collection_name(), cls.query_string(query_options))
 
     @classmethod
-    def element_path(cls, id, **query_options):
-        return '/%s/%s%s' % (cls.collection_name(), id, cls.query_string(query_options))
+    def element_path(cls, identifier, **query_options):
+        """Path to the element API endpoint."""
+        return '/%s/%s%s' % (cls.collection_name(), identifier, cls.query_string(query_options))
 
     @classmethod
-    def _request(cls, method, path, **kwargs):
+    def request(cls, method, path, **kwargs):
+        """Request an API endpoint."""
         if hasattr(cls.Meta, 'auth_type'):
             if cls.Meta.auth_type == 'basic':
                 auth_class = requests.auth.HTTPBasicAuth
