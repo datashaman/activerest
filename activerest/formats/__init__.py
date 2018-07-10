e1df79d51c9584ce18ae70bf82076012c6cec3cc
datashaman <marlinf@datashaman.com>
2018-07-10 18:37:13 +0200

    refactor

--- activerest/formats/__init__.py
+++ activerest/formats/__init__.py
@@ -1,5 +1,7 @@
 def remove_root(data):
-    if isinstance(data, dict) and len(data) == 1 and isinstance(list(data.values())[0], list):
-        return list(data.values())[0]
+    if isinstance(data, dict) and len(data) == 1:
+        data = list(data.values())[0]
+        if isinstance(data, list):
+            return data
     else:
         return data
