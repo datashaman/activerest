6be5b363e1ca8015a3d935cf8bbaf71c99a61fba
datashaman <marlinf@datashaman.com>
2018-07-11 21:34:57 +0200

    up coverage

--- activerest/formats/json_format.py
+++ activerest/formats/json_format.py
@@ -10,9 +10,11 @@ def mime_type():
     return 'application/json'
 
 def encode(data, **kwargs):
-    if 'indent' not in kwargs:
-        kwargs['indent'] = 4
-    return json.dumps(data, **kwargs)
+    encode_kwargs = {
+        'indent': 4,
+    }
+    encode_kwargs.update(kwargs)
+    return json.dumps(data, **encode_kwargs)
 
 def decode(text):
     return remove_root(json.loads(text))
--- activerest/formats/xml_format.py
+++ activerest/formats/xml_format.py
@@ -10,13 +10,12 @@ def mime_type():
     return 'application/xml'
 
 def encode(data, **kwargs):
-    if 'full_document' not in kwargs:
-        kwargs['full_document'] = False
-
-    if 'pretty' not in kwargs:
-        kwargs['pretty'] = True
-
-    return xmltodict.unparse(data, **kwargs)
+    encode_kwargs = {
+        'full_document': False,
+        'pretty': True,
+    }
+    encode_kwargs.update(kwargs)
+    return xmltodict.unparse(data, **encode_kwargs)
 
 def decode(text):
     return remove_root(xmltodict.parse(text))
