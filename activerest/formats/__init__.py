36ef3623a30d75fc7932da04402686a3712349b7
datashaman <marlinf@datashaman.com>
2018-07-11 08:14:22 +0200

    remove_root fix

--- activerest/formats/__init__.py
+++ activerest/formats/__init__.py
@@ -1,7 +1,6 @@
 def remove_root(data):
     if isinstance(data, dict) and len(data) == 1:
-        data = list(data.values())[0]
-        if isinstance(data, list):
-            return data
-    else:
-        return data
+        first_value = list(data.values())[0]
+        if isinstance(first_value, (dict, list)):
+            return first_value
+    return data
--- tests/formats_test.py
+++ tests/formats_test.py
@@ -3,10 +3,15 @@ from unittest import TestCase
 
 
 class FormatsTest(TestCase):
-    def test_remove_root_with_single_root(self):
+    def test_remove_root_with_single_list(self):
         values = [{'attr': 'value'}]
         data = {'values': values}
         self.assertEqual(values, remove_root(data))
+
+    def test_remove_root_with_single_dict(self):
+        value = {'attr': 'value'}
+        data = {'value': value}
+        self.assertEqual(value, remove_root(data))
 
     def test_remove_root_with_multiple_root(self):
         data = {'values': {}, 'other': {}}
